4353236b24f5fa65d70de41edeafcbf8c456d2d9
vesperalwall860 <vesperalwall860@gmail.com>
2017-04-30 10:47:29 +0300

    Back to the try..except construction

--- project/main/events.py
+++ project/main/events.py
@@ -9,11 +9,9 @@ clients = {}
 @socketio.on('hello')
 def handle_hello(message):
     print('received hello message: ' + str(message['data']))
-    # try:
+
     if current_user.is_authenticated:
         join_room(current_user.id)
-    # except:
-    #     pass
 
 
 @socketio.on('disconnect')
