e208c5cfa469f69faae863762dc846140df47425
Abhishek <AbhiProjects@users.noreply.github.com>
2017-05-15 14:09:43 +0530

    Error 403 - Forbidden Resource added

--- project/main/errors.py
+++ project/main/errors.py
@@ -21,3 +21,13 @@ def internal_server_error(e):
         response.status_code = 500
         return response
     return render_template('500.html'), 500
+
+
+@main.app_errorhandler(403)
+def forbidden(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden'})
+        response.status_code = 403
+        return response
+    return render_template('403.html'), 403
